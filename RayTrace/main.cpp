121eeacdcba580f601c1f3af7aad1d0091e582df
z55250825 <z55250825@163.com>
2017-05-28 23:52:02 +0800

    Initial Commit

---
+++ RayTrace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTrace
+//
+//  Created by z55250825 on 2017/5/28.
+//  Copyright © 2017年 z55250825. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
